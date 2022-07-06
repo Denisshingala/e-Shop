fde70fd88ed4ae7b6ca8a44dfd03ecefa28315fe
Shubham <dusaneshubham>
2022-07-06 12:39:06 +0530

    footer

--- user/product-detail.php
+++ user/product-detail.php
@@ -17,14 +17,14 @@ if (!isset($_GET['pid'])) {
     $stmt->close();
 }
 
-if(isset($_POST['review-btn']) && $_POST['review-description'] != '') {
+if (isset($_POST['review-btn']) && $_POST['review-description'] != '') {
     $description = $conn->real_escape_string($_POST['review-description']);
     $today = date("Y-m-d");
 
     $sql = "INSERT INTO reviews (product_id, user_id, description, review_date) VALUES (?,?,?,?)";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("iiss", $productID, $_SESSION['user_id'], $description, $today);
-    if($stmt->execute())
+    if ($stmt->execute())
         echo "Reviewed";
     else
         echo "Error in review";
@@ -240,27 +240,27 @@ require('./action/add-cart.php');
                     <div class="row p-3">
                         <div class="col-md-6">
 
-                        <?php
-                        $noOfReviews < 3 ? $n = $noOfReviews : $n = 3;
-                        for($i = 0; $i < $n; ++$i) {
-                            $row = $result->fetch_assoc();
-                            if($row['profile_image'] === '')
-                                $src = "https://bootdey.com/img/Content/avatar/avatar7.png";
-                            else 
-                                $src = "../" . $row['profile_image'];
+                            <?php
+                            $noOfReviews < 3 ? $n = $noOfReviews : $n = 3;
+                            for ($i = 0; $i < $n; ++$i) {
+                                $row = $result->fetch_assoc();
+                                if ($row['profile_image'] === '')
+                                    $src = "https://bootdey.com/img/Content/avatar/avatar7.png";
+                                else
+                                    $src = "../" . $row['profile_image'];
 
-                            echo '<div class="media mb-4">
-                                <img src="'.$src. '" alt="Image" class="img-fluid mr-3 mt-1" style="width: 45px; height: 45px; border-radius:50%;">
+                                echo '<div class="media mb-4">
+                                <img src="' . $src . '" alt="Image" class="img-fluid mr-3 mt-1" style="width: 45px; height: 45px; border-radius:50%;">
                                 <div class="media-body">
-                                    <h6>'.$row['name'].'<small> - <i>'.$row['review_date'].'</i></small></h6>
-                                    <p>'.$row['description'].'</p>
+                                    <h6>' . $row['name'] . '<small> - <i>' . $row['review_date'] . '</i></small></h6>
+                                    <p>' . $row['description'] . '</p>
                                 </div>
                             </div>';
-                        }
-                        
-                        if($noOfReviews > 3) 
-                            echo '<a href="" style="float:right; font-size:18px;">More Reviews >></a>';
-                        ?>  
+                            }
+
+                            if ($noOfReviews > 3)
+                                echo '<a href="review.php" style="float:right; font-size:18px;">More Reviews >></a>';
+                            ?>
                         </div>
 
                         <?php
@@ -364,91 +364,12 @@ require('./action/add-cart.php');
 
 
     <!-- Footer Start -->
-    <div class="container-fluid bg-secondary text-dark mt-5 pt-5">
-        <div class="row px-xl-5 pt-5">
-            <div class="col-lg-4 col-md-12 mb-5 pr-3 pr-xl-5">
-                <a href="" class="text-decoration-none">
-                    <h1 class="mb-4 display-5 font-weight-semi-bold"><span class="text-primary font-weight-bold border border-white px-3 mr-1">E</span>Shopper</h1>
-                </a>
-                <p>Dolore erat dolor sit lorem vero amet. Sed sit lorem magna, ipsum no sit erat lorem et magna ipsum dolore amet erat.</p>
-                <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>123 Street, New York, USA</p>
-                <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>info@example.com</p>
-                <p class="mb-0"><i class="fa fa-phone-alt text-primary mr-3"></i>+012 345 67890</p>
-            </div>
-            <div class="col-lg-8 col-md-12">
-                <div class="row">
-                    <div class="col-md-4 mb-5">
-                        <h5 class="font-weight-bold text-dark mb-4">Quick Links</h5>
-                        <div class="d-flex flex-column justify-content-start">
-                            <a class="text-dark mb-2" href="index.php"><i class="fa fa-angle-right mr-2"></i>Home</a>
-                            <a class="text-dark mb-2" href="shop.html"><i class="fa fa-angle-right mr-2"></i>Our
-                                Shop</a>
-                            <a class="text-dark mb-2" href="detail.html"><i class="fa fa-angle-right mr-2"></i>Shop
-                                Detail</a>
-                            <a class="text-dark mb-2" href="cart.html"><i class="fa fa-angle-right mr-2"></i>Shopping
-                                Cart</a>
-                            <a class="text-dark mb-2" href="checkout.html"><i class="fa fa-angle-right mr-2"></i>Checkout</a>
-                            <a class="text-dark" href="contact.html"><i class="fa fa-angle-right mr-2"></i>Contact
-                                Us</a>
-                        </div>
-                    </div>
-                    <div class="col-md-4 mb-5">
-                        <h5 class="font-weight-bold text-dark mb-4">Quick Links</h5>
-                        <div class="d-flex flex-column justify-content-start">
-                            <a class="text-dark mb-2" href="index.html"><i class="fa fa-angle-right mr-2"></i>Home</a>
-                            <a class="text-dark mb-2" href="shop.html"><i class="fa fa-angle-right mr-2"></i>Our
-                                Shop</a>
-                            <a class="text-dark mb-2" href="detail.html"><i class="fa fa-angle-right mr-2"></i>Shop
-                                Detail</a>
-                            <a class="text-dark mb-2" href="cart.html"><i class="fa fa-angle-right mr-2"></i>Shopping
-                                Cart</a>
-                            <a class="text-dark mb-2" href="checkout.html"><i class="fa fa-angle-right mr-2"></i>Checkout</a>
-                            <a class="text-dark" href="contact.html"><i class="fa fa-angle-right mr-2"></i>Contact
-                                Us</a>
-                        </div>
-                    </div>
-                    <div class="col-md-4 mb-5">
-                        <h5 class="font-weight-bold text-dark mb-4">Newsletter</h5>
-                        <form action="">
-                            <div class="form-group">
-                                <input type="text" class="form-control border-0 py-4" placeholder="Your Name" required="required" />
-                            </div>
-                            <div class="form-group">
-                                <input type="email" class="form-control border-0 py-4" placeholder="Your Email" required="required" />
-                            </div>
-                            <div>
-                                <button class="btn btn-primary btn-block border-0 py-3" type="submit">Subscribe
-                                    Now</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row border-top border-light mx-xl-5 py-4">
-            <!-- <div class="col-md-6 px-xl-0"> -->
-            <p class="text-center text-dark">
-                &copy; <a class="text-dark font-weight-semi-bold" href="#">e-shop</a>. All Rights Reserved. Designed by
-                <a class="text-dark font-weight-semi-bold" href="">VGEC student</a>
-            </p>
-        </div>
-    </div>
+    <?php include('./utilities/footer.php'); ?>
     <!-- Footer End -->
 
 
     <!-- Back to Top -->
     <a href="#" class="btn btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
-
-
-    <!-- JavaScript Libraries -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-    <script src="lib/easing/easing.min.js"></script>
-    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-
-    <!-- Contact Javascript File -->
-    <script src="mail/jqBootstrapValidation.min.js"></script>
-    <script src="mail/contact.js"></script>
 
     <!-- Template Javascript -->
     <script src="js/main.js"></script>
---
+++ user/review.php
@@ -0,0 +1,101 @@
+<?php
+include('../configuration/config.php');
+
+if (isset($_GET['pid']))
+    $productID = $_GET['pid'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>e-shop</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap 4 Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script>
+        if (window.history.replaceState) {
+            window.history.replaceState(null, null, window.location.href);
+        }
+    </script>
+</head>
+
+<body>
+    <!-- Navbar Start -->
+    <?php include('./utilities/navbar.php'); ?>
+    <!-- Navbar End -->
+
+    <hr>
+
+    <div class="container-fluid px-xl-5">
+        <?php
+        $sql = "SELECT title FROM product WHERE product_id=?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("i", $productID);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+        echo '<h4>Reviews for ' . $row['title'] . '</h4>'
+        ?>
+
+        <div id="review-section">
+            <div class="row p-3">
+                <div class="col-md-12">
+
+                    <?php
+                    $sql = "SELECT user.profile_image, user.name, reviews.review_date, reviews.description FROM reviews JOIN user ON user.user_id = reviews.user_id WHERE product_id=?";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->bind_param("i", $productID);
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+                    while ($row = $result->fetch_assoc()) {
+                        if ($row['profile_image'] === '')
+                            $src = "https://bootdey.com/img/Content/avatar/avatar7.png";
+                        else
+                            $src = "../" . $row['profile_image'];
+
+                        echo '<div class="media mb-4">
+                                <img src="' . $src . '" alt="Image" class="img-fluid mr-3 mt-1" style="width: 45px; height: 45px; border-radius:50%;">
+                                <div class="media-body">
+                                    <h6>' . $row['name'] . '<small> - <i>' . $row['review_date'] . '</i></small></h6>
+                                    <p>' . $row['description'] . '</p>
+                                </div>
+                            </div>';
+                    }
+                    ?>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Footer Start -->
+    <?php include('./utilities/footer.php'); ?>
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
